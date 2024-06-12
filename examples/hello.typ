de94b00428b9e9d93664a8610b5d3c33cf77cfd3
Nanozuki Crows <nanozuki.crows@gmail.com>
2024-06-12 15:59:22 +0800

    feat: init develop env, try typst parser

---
+++ examples/hello.typ
@@ -0,0 +1,11 @@
+#title[hello]
+
+This is a simple hello world plugin for neovim.
+
+= Math module
+
+def(fun: HelloMathAdd)
+
+#par(indent: 8)
+
+This is a paragrap with 8 spaces of indentation.
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::fs;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <filename>", args[0]);
+        process::exit(1);
+    }
+    let filename = &args[1];
+
+    let content = fs::read_to_string(filename).unwrap_or_else(|err| {
+        eprintln!("Error reading file '{}': {}", filename, err);
+        process::exit(1);
+    });
+
+    let parsed_data = typst_syntax::parse(&content);
+    println!("{:#?}", parsed_data);
+}
